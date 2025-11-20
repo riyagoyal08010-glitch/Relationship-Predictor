ea334d9f1414681ff39f4d0aa3b86d88c18e2d0e
Riya <riyagoyal.08010@gmail.com>
2025-11-21 02:11:21 +0530

    v18

--- StreamlitDeploys/dashboard.py
+++ StreamlitDeploys/dashboard.py
@@ -21,8 +21,10 @@ st.markdown("""
 # --- Data Loading ---
 @st.cache_data
 def load_data():
-    data_path = pd.read_csv(r"C:\Users\HP\Downloads\train.csv")
-    lookup_path = pd.read_csv(r"C:\Users\HP\Downloads\feature_lookup.csv")
+    data = pd.read_csv(r"C:\Users\HP\Downloads\train.csv")
+    data_path = data.to_csv()
+    lookup = pd.read_csv(r"C:\Users\HP\Downloads\feature_lookup.csv")
+    lookup_path = lookup.to_csv
     
     if not os.path.exists(data_path):
         return None, None, None
